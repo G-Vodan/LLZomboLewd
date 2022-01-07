f3ad338afaecc3b334cf41966f6991e2d7f2c9ec
QueuedResonance <96891007+QueuedResonance@users.noreply.github.com>
2022-01-07 03:55:54 -0500

    Defeat Minor Revamp
    Defeat no longer uses scratch resistance, instead does an order-of-operation stripping. In a nutshell, the more clothes you have, the less likely you will get grabbed for funsies.

--- ZomboLewdDefeat/media/lua/client/ZomboLewdDefeat/ZomboLewdDefeatModOptions.lua
+++ ZomboLewdDefeat/media/lua/client/ZomboLewdDefeat/ZomboLewdDefeatModOptions.lua
@@ -1,30 +1,25 @@
+local ZomboLewdDefeat = require("ZomboLewdDefeat/ZomboLewdDefeat")
 local ZomboLewdDefeatConfig = ZomboLewdDefeatConfig
 
 --- Setup ModOptions
 local ZomboLewdDefeatModOptions = {
 	options = {
 		dropdown1 = 2,
-		dropdown2 = 2,
+		dropdown2 = 1,
 		box1 = true,
-		dropdown3 = 2,
 		box2 = true,
-		dropdown4 = 2,
 		box3 = true,
-		dropdown5 = 1,
 		box4 = true,
-		dropdown6 = 1,
+		dropdown3 = 5
 	},
 	names = {
 		dropdown1 = getText("IGUI_Cooldown_Grabs"),
-		dropdown2 = getText("IGUI_Minimum_Scratch"),
-		box1 = getText("IGUI_Can_Damage_Belt"),
-		dropdown3 = "Belt Condition Damage Value",
-		box2 = getText("IGUI_Can_Damage_Pants"),
-		dropdown4 = "Pants Condition Damage Value",
-		box3 = getText("IGUI_Can_Damage_Tshirt"),
-		dropdown5 = "T-Shirt Condition Damage Value",
-		box4 = getText("IGUI_Can_Damage_Shoes"),
-		dropdown6 = "Shoes Condition Damage Value",
+		dropdown2 = getText("IGUI_Condition_Damage"),
+		box1 = getText("IGUI_Disable_Equipment_Top_Damage"),
+		box2 = getText("IGUI_Disable_Equipment_Bottom_Damage"),
+		box3 = getText("IGUI_Disable_Equipment_Full_Damage"),
+		box4 = getText("IGUI_Disable_Equipment_Hole_Damage"),
+		dropdown3 = getText("IGUI_Hole_Damage_Value")
 	},
 	mod_id = "ZombolewdDefeat",	
 	mod_shortname = "ZomboLewd Defeat Options",
@@ -51,55 +46,29 @@ if ModOptions and ModOptions.getInstance then
 		table.insert(dropdown, "2000 Ticks")
 	end
 
-	do
-		local dropdown = ZomboLewdDefeatModOptions.options_data.dropdown2
-		table.insert(dropdown, "10 Scratch Resistance")
-		table.insert(dropdown, "20 Scratch Resistance")
-		table.insert(dropdown, "30 Scratch Resistance")
-		table.insert(dropdown, "40 Scratch Resistance")
-		table.insert(dropdown, "50 Scratch Resistance")
-		table.insert(dropdown, "60 Scratch Resistance")
-		table.insert(dropdown, "70 Scratch Resistance")
-		table.insert(dropdown, "80 Scratch Resistance")
-		table.insert(dropdown, "90 Scratch Resistance")
-		table.insert(dropdown, "100 Scratch Resistance")
-	end
-
 	--- CLOTHING DAMAGE
 	do
+		local dropdown = ZomboLewdDefeatModOptions.options_data.dropdown2
+		table.insert(dropdown, "-1 Condition Damage")
+		table.insert(dropdown, "-2 Condition Damage")
+		table.insert(dropdown, "-3 Condition Damage")
+		table.insert(dropdown, "-4 Condition Damage")
+		table.insert(dropdown, "-5 Condition Damage")
+	end
+
+	--- CLOTHING HOLES
+	do
 		local dropdown = ZomboLewdDefeatModOptions.options_data.dropdown3
-		table.insert(dropdown, "-1 Condition Damage per grab")
-		table.insert(dropdown, "-2 Condition Damage per grab")
-		table.insert(dropdown, "-3 Condition Damage per grab")
-		table.insert(dropdown, "-4 Condition Damage per grab")
-		table.insert(dropdown, "-5 Condition Damage per grab")
-	end
-
-	do
-		local dropdown = ZomboLewdDefeatModOptions.options_data.dropdown4
-		table.insert(dropdown, "-1 Condition Damage per grab")
-		table.insert(dropdown, "-2 Condition Damage per grab")
-		table.insert(dropdown, "-3 Condition Damage per grab")
-		table.insert(dropdown, "-4 Condition Damage per grab")
-		table.insert(dropdown, "-5 Condition Damage per grab")
-	end
-
-	do
-		local dropdown = ZomboLewdDefeatModOptions.options_data.dropdown5
-		table.insert(dropdown, "-1 Condition Damage per grab")
-		table.insert(dropdown, "-2 Condition Damage per grab")
-		table.insert(dropdown, "-3 Condition Damage per grab")
-		table.insert(dropdown, "-4 Condition Damage per grab")
-		table.insert(dropdown, "-5 Condition Damage per grab")
-	end
-
-	do
-		local dropdown = ZomboLewdDefeatModOptions.options_data.dropdown6
-		table.insert(dropdown, "-1 Condition Damage per grab")
-		table.insert(dropdown, "-2 Condition Damage per grab")
-		table.insert(dropdown, "-3 Condition Damage per grab")
-		table.insert(dropdown, "-4 Condition Damage per grab")
-		table.insert(dropdown, "-5 Condition Damage per grab")
+		table.insert(dropdown, "10%")
+		table.insert(dropdown, "20%")
+		table.insert(dropdown, "30%")
+		table.insert(dropdown, "40%")
+		table.insert(dropdown, "50%")
+		table.insert(dropdown, "60%")
+		table.insert(dropdown, "70%")
+		table.insert(dropdown, "80%")
+		table.insert(dropdown, "90%")
+		table.insert(dropdown, "100%")
 	end
 
 	ModOptions:loadFile()
